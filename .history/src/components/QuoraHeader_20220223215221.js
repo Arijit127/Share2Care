be6aae8447dcd7cb0e8b7b702430c1b25e70832d
AkkiPaul2000 <skypaul2000@gmail.com>
2022-03-12 16:07:26 +0530

    Delete History

--- .history/src/components/QuoraHeader_20220223215221.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function QuoraHeader() {
-  return (
-    <div>Hello this is the header</div>
-  )
-}
-
-export default QuoraHeader
--- .history/src/components/Quora_20220223215009.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Quora() {
-  return (
-    <div className='quora'>Quora</div>
-  )
-}
-
-export default Quora
